1cd86b419c1d265e0299308be247874806756e37
Brian Silverman <bri@raspberrystem.com>
2015-07-10 10:17:47 -0400

    clear cache on chromium start
    Cache corruption on sudden shutdown (or other cases) can cause missing file
    or broken image links.  So start with a clean cache on every boot.

--- start_client.sh
+++ start_client.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Cache corruption on sudden shutdown (or other cases) can cause missing file
+# or broken image links.  So start with a clean cache on every boot.
+rm -rf ~/.cache/chromium
+
 chromium --kiosk localhost
