ef2b2634024ebb1577dd4483c875dd57ddee9f05
Brian Silverman <bri@raspberrystem.com>
2015-07-11 13:44:06 -0400

    disable chrmoium message "didn't shut down correctly" after hard shutdown

--- start_client.sh
+++ start_client.sh
@@ -2,6 +2,13 @@
 
 # Cache corruption on sudden shutdown (or other cases) can cause missing file
 # or broken image links.  So start with a clean cache on every boot.
-rm -rf ~/.cache/chromium
+rm -rf $HOME/.cache/chromium
 
+# A hard shutdown causes Chromium to report "didn't shut down correctly"...
+# Clean files responsible for this (Note: this method is ad-hoc, there's no
+# clearly defined way to do this).
+rm -f $HOME/.config/chromium/Default/Preferences
+rm -f $HOME/.config/chromium/SingletonLock
+
+# Run!
 chromium --kiosk localhost
